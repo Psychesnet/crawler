8b0b470625c9812505b7f4806a226f8fb924fe1e
eric hsieh <psychesnet@gmail.com>
2021-02-26 14:43:37 +0800

    refine macd

--- macd.py
+++ macd.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+import talib
 
 stock_num = "2330"
 stockNo = "{}.TW".format(stock_num)
@@ -13,10 +14,9 @@ stock_df = stock_df.reset_index()
 print("參考交易日天數: {} 天".format(len(stock_df)))
 stock_df.head()
 
-stock_df['EMA_12'] = stock_df['Close'].ewm(span=12, adjust=False).mean()
-stock_df['EMA_26'] = stock_df['Close'].ewm(span=26, adjust=False).mean()
-stock_df['DIF'] = stock_df['EMA_12'] - stock_df['EMA_26']
-stock_df['MACD'] = stock_df['DIF'].ewm(span=9, adjust=False).mean()
+stock_df["MA7"] = talib.MA(stock_df['Close'], timeperiod=7)
+stock_df["MA30"] = talib.MA(stock_df['Close'], timeperiod=30)
+stock_df['MACD'], stock_df['MACD_Signal'], stock_df['MACD_Hist'] = talib.MACD(stock_df['Close'])
 stock_df.head()
 
 fig,ax1 = plt.subplots()
@@ -26,8 +26,9 @@ plt.xlabel('Date')
 ax2 = ax1.twinx()
 # macd & dif
 ax1.set_ylabel('MACD', color='black')
-ax1.plot(stock_df['Date'], stock_df['DIF'], color='gold', label="DIF", alpha=1)
-ax1.plot(stock_df['Date'], stock_df['MACD'], color='coral', label="MACD", alpha=1)
+ax1.plot(stock_df['Date'], stock_df['MACD'], color='yellow', label="MACD", alpha=2)
+ax1.plot(stock_df['Date'], stock_df['MACD_Signal'], color='red', label="signal", alpha=2)
+ax1.bar(stock_df['Date'], stock_df['MACD_Hist']*3, color='blue', label="hist")
 ax1.legend(loc="lower left")
 ax1.tick_params(axis='y', labelcolor='black')
 ax1.grid(True)
@@ -35,8 +36,8 @@ ax1.grid(True)
 ax2.set_ylabel('Price', color='black')
 ax2.plot(stock_df['Date'], stock_df['Close'], color='black', label="Price", linestyle=':', alpha=0.8)
 # ma: 移動平均線(Moving Average)
-stock_df['MA_7'] = stock_df['Close'].rolling(7).mean()
-ax2.plot(stock_df['Date'], stock_df['MA_7'], color='blue', label="MA", linestyle='-', alpha=0.8)
+ax2.plot(stock_df['Date'], stock_df['MA7'], color='purple', label="MA7", alpha=1, linestyle='-')
+ax2.plot(stock_df['Date'], stock_df['MA30'], color='grey', label="MA30", alpha=1, linestyle='-')
 ax2.legend(loc="upper left")
 ax2.tick_params(axis='y', labelcolor='black')
 fig.autofmt_xdate()
